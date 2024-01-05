c85c5d76df14d9b3dc4b50f2d82f5cc9462cc90e
Ophelie <freville.ophelie@neuf.fr>
2024-01-05 15:22:06 +0100

    Nav

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -2,18 +2,23 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class ContactController
+class ContactController extends AbstractController
 {
     #[Route('/contact', 'contact')]
-    public function contact(): Response
-    {
-        $variable= 'Bienvenue sur la page Contact';
 
-        return new Response(
-            '<html><body><h1>'.$variable.'</h1></body></html>'
-        );
+    public function index(): Response
+    
+    {
+        // $variable= 'Bienvenue sur la page Contact';
+
+        // return new Response(
+        //     '<html><body><h1>'.$variable.'</h1></body></html>'
+        // );
+
+        return $this->render('contact/index.html.twig');
     }
 }
